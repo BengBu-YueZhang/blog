11957877a640c52fec46ee96150bdbc3ab184ed8
BengBu-YueZhang <zhangyue_coder@163.com>
2019-06-30 17:20:33 +0800

    添加scss

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.scss';
+
+const Header: React.FC = () => {
+  return (
+    <header>
+      <h1 className='logo'>123</h1>
+    </header>
+  );
+}
+
+export default Header;
