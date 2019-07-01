a15579b96e98d255d54505872f49378d2a32850c
BengBu-YueZhang <zhangyue_coder@163.com>
2019-07-01 17:19:19 +0800

    更新代码

--- src/base/Animate/AnimationQueue.tsx
+++ src/base/Animate/AnimationQueue.tsx
@@ -1,5 +1,5 @@
 // Thank https://github.com/soWhiteSoColl/dodo-ui/blob/master/src/animate-queue/index.tsx
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Animation, { IAnimation } from './Animation';
 
 export interface IAnimationQueue extends IAnimation {
@@ -8,13 +8,15 @@ export interface IAnimationQueue extends IAnimation {
 }
 
 const AnimationQueue: any = (props: IAnimationQueue) => {
-
   const [pointer, setPointer] = useState(0)
+
+  const timer = useRef(0)
 
   const {
     children,
     animation = true,
     duration = 200,
+    interval = 100,
     timingFunction = 'ease-in-out'
   } = props;
 
@@ -30,15 +32,45 @@ const AnimationQueue: any = (props: IAnimationQueue) => {
   to = { opacity: 1, ...to };
   from = { opacity: 0, ...from };
 
-  const handleAnimationStateChange = () => {
+  const handleAnimationStateChange = (animation: boolean): void => {
+    const len = children.length;
+
+    clearTimeout(timer.current)
+
+    const loop = function () {
+      if (!animation && pointer <= 0) {
+        setPointer(0)
+      } else if (animation && pointer >= len) {
+        setPointer(len)
+      } else {
+        setPointer(prevPointer => {
+          if (animation) {
+            return prevPointer + 1
+          } else {
+            return prevPointer - 1
+          }
+        })
+        timer.current = window.setTimeout(loop, interval);
+      }
+    }
+    loop();
   }
 
   useEffect(() => {
+    handleAnimationStateChange(animation)
   }, [animation, children.length])
 
-  return (
-    <div></div>
-  )
+  return React.Children.map(children, (child: any, index) => {
+    return (
+      <Animation
+        animation={pointer > index}
+        to={to}
+        from={from}
+        duration={duration}
+        timingFunction={timingFunction}
+      >{ child }</Animation>
+    )
+  })
 }
 
 export default AnimationQueue;
