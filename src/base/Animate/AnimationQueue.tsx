df0bc39f931ae0a392b1d4af1db8594e61212a3f
BengBu-YueZhang <zhangyue_coder@163.com>
2019-07-01 16:46:19 +0800

    更新代码

--- src/base/Animate/AnimationQueue.tsx
+++ src/base/Animate/AnimationQueue.tsx
@@ -1,8 +1,43 @@
-import React from 'react';
+// Thank https://github.com/soWhiteSoColl/dodo-ui/blob/master/src/animate-queue/index.tsx
+import React, { useState, useEffect } from 'react';
+import Animation, { IAnimation } from './Animation';
 
-const AnimationQueue: React.FC = () => {
+export interface IAnimationQueue extends IAnimation {
+  interval: number;
+  children: React.ReactNodeArray
+}
+
+const AnimationQueue: any = (props: IAnimationQueue) => {
+
+  const [pointer, setPointer] = useState(0)
+
+  const {
+    children,
+    animation = true,
+    duration = 200,
+    timingFunction = 'ease-in-out'
+  } = props;
+
+  let {
+    to = {
+      opacity: 1
+    },
+    from = {
+      opacity: 0
+    }
+  } = props;
+
+  to = { opacity: 1, ...to };
+  from = { opacity: 0, ...from };
+
+  const handleAnimationStateChange = () => {
+  }
+
+  useEffect(() => {
+  }, [animation, children.length])
+
   return (
-    <div>About</div>
+    <div></div>
   )
 }
 
